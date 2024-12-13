96c59aa03c43de7ce0545f10686b2c3b0756a7a2
Felix Schulz <felix.schulz@stud.it.htl-hl.ac.at>
2024-12-13 11:08:44 +0100

    d6#1 finished

--- day6/part1.py
+++ day6/part1.py
@@ -2,15 +2,35 @@ map = list(map(list, open('data.txt').read().splitlines()))
 for x, line in enumerate(map):
     if '^' in line:
         guard_pos = [x, line.index('^')]
+
+
+for line in map:
+    print(line)
 print("guard starts at: ", guard_pos)
 
-direction = [0, 1]
+direction = [-1, 0]
 
 def rotate_90_deg(direction):
-    return
-print("try +1: " ,guard_pos + direction)
+    return [direction[1], -direction[0]]
 
-while(guard_pos[0] < len(map) and guard_pos[1] < len(map[0])):
-    if map[guard_pos[0] + direction[0], guard_pos[1] + direction[1]] :
+while(0 <= guard_pos[0] < len(map) and 0 <= guard_pos[1] < len(map[0])):
+    try:
+        if map[guard_pos[0] + direction[0]] [guard_pos[1] + direction[1]] == '#' :
+            direction = rotate_90_deg(direction)
+    except:
         break
-        # ka muss noch machen
+    map[guard_pos[0]][guard_pos[1]] = 'X'
+    
+    guard_pos[0] += direction[0]
+    guard_pos[1] += direction[1]
+    map[guard_pos[0]][guard_pos[1]] = '^'
+    print(guard_pos)
+for line in map:
+    print(line)
+print("guard has left the map at:", guard_pos, "and in direction: ", direction)
+total = 1
+for line in map:
+    for tile in line:
+        if tile == 'X':
+            total += 1
+print("guard walked ", total, " tiles")
